65753d34937874590c839e52a7def8694d3e6591
Priyobrato Das <priyodas12@gmail.com>
2020-05-04 00:35:42 +0530

    web app int added added base controller

---
+++ src/main/java/io/priyospace/controller/InitController.java
@@ -0,0 +1,17 @@
+package io.priyospace.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class InitController {
+    /*http://localhost:9080/daily-todo-study/api/hello*/
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
