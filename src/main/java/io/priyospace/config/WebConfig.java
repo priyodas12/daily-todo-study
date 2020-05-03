2f6b0af2efb38274ff013fbe0ef590e72dd93004
Priyobrato Das <priyodas12@gmail.com>
2020-05-03 23:40:41 +0530

    webconfig addded

---
+++ src/main/java/io/priyospace/config/WebConfig.java
@@ -0,0 +1,12 @@
+package io.priyospace.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"io.priyospace"})
+public class WebConfig {
+
+}
